7f64712dd5a84afbd773d8bd76f02eb4480d5bf3
Pierre Pineau <pierrepineau.pro@gmail.com>
2022-07-27 22:13:08 +0200

    Update SendOrderCheckout

---
+++ app/Mail/SendOrderCheckout.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\User;
+use App\Models\Order;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class SendOrderCheckout extends Mailable
+{
+    use Queueable, SerializesModels;
+
+
+    public User $user;
+    public Order $order;
+
+    public function __construct(User $user, Order $order)
+    {
+        $this->user = $user;
+        $this->order = $order;
+    }
+
+    public function build()
+    {
+        return $this->view('mails.OrderCheckout', ['user' => $this->user, 'order' => $this->order]);
+    }
+}
